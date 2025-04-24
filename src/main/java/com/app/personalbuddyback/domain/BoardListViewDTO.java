1c4da6657306949773bf696e4cc26346cd01cb83
jiihyun12 <j22h.h0h@gmail.com>
2025-04-24 23:53:48 +0900

    지현 보드 DTO 작업

--- src/main/java/com/app/personalbuddyback/domain/BoardListViewDTO.java
+++ src/main/java/com/app/personalbuddyback/domain/BoardListViewDTO.java
@@ -1,9 +1,27 @@
+// 보드 메인 리스트
+
 package com.app.personalbuddyback.domain;
 
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Data
 @Component
 public class BoardListViewDTO {
+    private Long id; // 게시글 ID
+    private String boardTitle; // 게시글 제목
+    private String boardImgPath; // 게시글 이미지 경로
+    private String boardImgName; // 게시글 이미지 이름
+    private String boardHashtag; // 게시글 해시태그
+    private String boardContentViews; // 게시글 조회수
+    private Long boardLikeCount; // 게시글 좋아요
+    private Date boardCreateDate; // 게시글 작성일+시간
+    private int boardCommentCount; // 댓글수
+    private Long memberId; // 게시글 작성자 ID
+    private String memberNickName; // 멤버 닉네임
+    private String memberImgPath; // 게시글 작성자 프로필 이미지 경로
+    private String memberImgName; // 게시글 작성자 프로필 이미지 이름
+
 }
