4024808372aba727777aea347e72b1864fbc1cde
kys3189 <kimys31892@gmail.com>
2025-04-25 17:36:02 +0900

    1.4.2

--- src/main/java/com/app/personalbuddyback/mapper/AlertMapper.java
+++ src/main/java/com/app/personalbuddyback/mapper/AlertMapper.java
@@ -1,8 +1,20 @@
 package com.app.personalbuddyback.mapper;
 
+import com.app.personalbuddyback.domain.AlertVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface AlertMapper {
-    public String getTime();
+//    알림 발생
+    public void insert(AlertVO alertVO);
+//    알림 리스트
+    public List<AlertVO> selectAll();
+//    알림 - 읽음 여부
+    public void update(Long id);
+//    알림 삭제
+    public void delete(Long id);
+//    알림 전체 삭제
+    public void deleteAll(Long receiverMemberId);
 }
