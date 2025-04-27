88c3c80581899f18389dc7d04c4a03ca81a4e99b
jiihyun12 <j22h.h0h@gmail.com>
2025-04-27 10:11:14 +0900

    DTO 작업 끝

--- src/main/java/com/app/personalbuddyback/domain/EventViewDTO.java
+++ src/main/java/com/app/personalbuddyback/domain/EventViewDTO.java
@@ -1,9 +1,21 @@
+// 이벤트 상세 페이지
+
 package com.app.personalbuddyback.domain;
 
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Data
 @Component
 public class EventViewDTO {
+    private Long id; // 이벤트 ID
+    private String eventTitle; // 이벤트 제목
+    private String eventDescription; // 이벤트 설명
+    private int eventViews; // 조회수
+    private int eventCommentCount; // 댓글수
+    private String eventStartDate; // 시작날짜
+    private String eventEndDate; // 종료 날짜
+    private int eventGetPoint; // 참여시 받을 포인트
 }
