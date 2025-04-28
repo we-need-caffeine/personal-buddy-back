a41af383cb22ed3679422174dd053ad8c127e42a
cutekang <rkddl94@gmail.com>
2025-04-28 09:35:38 +0900

    v1.4.7

--- src/main/java/com/app/personalbuddyback/mapper/TargetMapper.java
+++ src/main/java/com/app/personalbuddyback/mapper/TargetMapper.java
@@ -1,8 +1,18 @@
 package com.app.personalbuddyback.mapper;
 
+import com.app.personalbuddyback.domain.TargetVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TargetMapper {
-    public String getTime();
+
+//    목표테이블에 insert 되면, 기간에 따라 완료 횟수를 체크한다.
+//    따라서, 일정이 추가되면, target 테이블에 함께 insert 알맞는 정보를 insert 하여 정보를 기입
+    public void insert(TargetVO targetVO);
+
+//    일간 목표 확인
+    public int selectDailyTargetCountByCategoryAndMemberId(TargetVO targetVO);
+
+//    주간/월간 목표 확인
+    public int selectWeeklyMonthlyTargetCountByCategoryAndMemberId(TargetVO targetVO);
 }
