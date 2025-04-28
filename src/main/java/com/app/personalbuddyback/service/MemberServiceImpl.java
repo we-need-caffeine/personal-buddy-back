d2fdf8ad3ecb0130fd3e2cb2b1146a6bbaff0593
oasis1340 <oasis1340@gmail.com>
2025-04-28 09:11:25 +0900

    memberMapper 업데이트, MemberAPI 추가

--- src/main/java/com/app/personalbuddyback/service/MemberServiceImpl.java
+++ src/main/java/com/app/personalbuddyback/service/MemberServiceImpl.java
@@ -1,9 +1,19 @@
 package com.app.personalbuddyback.service;
 
+import com.app.personalbuddyback.domain.MemberVO;
+import com.app.personalbuddyback.repository.MemberDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
 public class MemberServiceImpl implements MemberService {
+
+    private final MemberDAO memberDAO;
+
+    public void join(MemberVO memberVO) {
+        memberDAO.insert(memberVO);
+    }
 }
