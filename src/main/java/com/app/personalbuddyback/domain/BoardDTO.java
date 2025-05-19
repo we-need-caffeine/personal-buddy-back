7b9c720d81ea85db4b34f2b66ccd84ad6e90a61f
sehwan-kim <codefuling@gmail.com>
2025-05-19 14:27:44 +0900

    0519 지현

---
+++ src/main/java/com/app/personalbuddyback/domain/BoardDTO.java
@@ -0,0 +1,27 @@
+package com.app.personalbuddyback.domain;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Data
+public class BoardDTO {
+    private int id;
+    private String boardTitle;
+    private String boardContent;
+    private Long boardContentViews;
+    private Date boardContentCreateDate;
+    private Date boardContentUpdateDate;
+    private Long boardLikeCount;
+    private Long boardCommentCount;
+    private String boardHashtag;
+    private String boardImgPath;
+    private String boardImgName;
+    private Long memberId;
+    private String memberEmail;
+    private String memberNickname;
+    private String memberImgPath;
+    private String memberImgName;
+}
