87bc22377f2631ea3ae249c44e7cc33086a0aaa6
kys3189 <kimys31892@gmail.com>
2025-05-29 09:56:02 +0900

    boardMapper, BoardListViewDTO 수정

--- src/main/java/com/app/personalbuddyback/domain/BoardListViewDTO.java
+++ src/main/java/com/app/personalbuddyback/domain/BoardListViewDTO.java
@@ -13,6 +13,7 @@ import java.util.Date;
 public class BoardListViewDTO {
     private Long id; // 게시글 ID
     private String boardTitle; // 게시글 제목
+    private String boardContent; // 게시글 내용
     private String boardImgPath; // 게시글 이미지 경로
     private String boardImgName; // 게시글 이미지 이름
     private String boardHashtag; // 게시글 해시태그
